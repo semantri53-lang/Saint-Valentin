9c57b09b8b475549a619ef98e9bf028c1e02da5f
semantri53-lang <semantri53@gmail.com>
2026-02-13 19:05:58 +0100

    Update main.js

--- main.js
+++ main.js
@@ -4,7 +4,8 @@ noBtn.addEventListener("mouseover", () => {
     const x = Math.random() * (window.innerWidth - 120);
     const y = Math.random() * (window.innerHeight - 60);
 
-    noBtn.style.position = "absolute";
+    noBtn.style.position = "fixed";
+    noBtn.style.zIndex = "999";
     noBtn.style.left = `${x}px`;
     noBtn.style.top = `${y}px`;
 });
